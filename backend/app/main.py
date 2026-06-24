538059279408672ddef0c59fe43954dbd38852d4
patrotakash1 <patrotakash1@gmail.com>
2026-06-24 14:17:24 +0530

    Allow all CORS origins

--- backend/app/main.py
+++ backend/app/main.py
@@ -108,11 +108,7 @@ app.include_router(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:3000",
-        "https://cineflow-ai-chi.vercel.app",
-        "https://cineflow-6wdhay838-reiner3.vercel.app",
-]),
+    allow_origins=["*"],),
 
 
 app.include_router(auth.router, prefix="/api/auth", tags=["auth"])
